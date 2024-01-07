141491e6ccc487bc0c727513c27213d8b1757391
Donald-Ok <144026151+Donald-Ok@users.noreply.github.com>
2024-01-07 17:26:20 +0300

    Csv postgres update one specific column values (#1)
    * Refactor Code
    
    * Add untested update postgres from csv
    
    * Tested csv_to_postgres_update_one_column

--- migration.py
+++ migration.py
@@ -6,15 +6,16 @@ if __name__ == '__main__':
     print('Welcome')
     print('Choose Migration Option:')
     print(' 1. CSV to Postgres')
+    print(' 2. CSV to Postgres Update One Specific Column Values')
+    csv_path = ''
     try:
         migration_option = int(input('Option: '))
     except ValueError:
         print('Invalid Migration Option')    
         quit()
-
     match migration_option:
         case 1:
-            csv_path = ''
+            csv_postgres_util = csv_to_postgres()  
             column_mapping_in_progress: False
             excel_db_column_mapping: dict = {}
             print("Enter the columns in your excel sheet with the columns in the db they should map to:")
@@ -32,68 +33,31 @@ if __name__ == '__main__':
                 continue_mapping: str = input('Enter Y for yes anything else for no: ')
                 column_mapping_in_progress = True if continue_mapping.lower() == 'y' else False
             print('The final mapping is:', excel_db_column_mapping)
-            print('Enter the file path')
-            incorrect_path = True
-            while incorrect_path:
-                print('Paste in/ Enter the path to the csv file')
-                print('The format should be like: C:\\\\file.csv')
-                csv_path: str = input('Paste in/ Enter the path to the csv file: ')
-                if csv_to_postgres.is_valid_csv(csv_path.strip()):
-                    incorrect_path = False
-                    break
-                else:
-                    print(f'{csv_path} is an invalid path')
-            incorrect_connection_string = True
-            while incorrect_connection_string:
-                db_host = input('Enter the database host: ')
-                db_username = input('Enter the database username: ')
-                db_password = input('Enter the database password: ')
-                db_name = input('Enter the database name: ')
-                invalid_port = True
-                while invalid_port:
-                    try:
-                        db_port = int(input('Enter the database port: '))
-                        invalid_port = False
-                        break
-                    except ValueError:
-                        print('The port number entered is invalid')     
-                is_valid_connection_string, connection_string, connection_error_thrown \
-                    = csv_to_postgres.test_connection_to_postgres_db({ 'host': f'{db_host}', 'u_name': f'{db_username}', 'pass': f'{db_password}','db_name': f'{db_name}', 'port': f'{db_port}'})
-                if (is_valid_connection_string):
-                    incorrect_connection_string = False
-                    break
-                else:
-                    print(f'The connection was not made due to error {connection_error_thrown}')
+            csv_path = csv_postgres_util.get_csv_file_path()
+            connection_string = csv_postgres_util.get_db_connection_string()
             database_table = input('Enter the name of the database table: ')
-            schema:str = input('Enter the target schema, if nothing is entered public will be the default schema: ')
-            csv_to_postgres.csv_to_postgres_append(
-                csv_file_path = csv_path.strip(),
-                db_connection_string = connection_string,
-                mapping = excel_db_column_mapping,
-                database_table = database_table ,
-                schema = schema if len(schema) >= 1 and schema.strip() != '' else 'public' 
+            schema: str = input('Enter the target schema, if nothing is entered public will be the default schema: ')
+            csv_postgres_util.csv_to_postgres_append(
+                csv_file_path=csv_path.strip(),
+                db_connection_string=connection_string,
+                mapping=excel_db_column_mapping,
+                database_table=database_table,
+                schema=schema if len(schema) >= 1 and schema.strip() != '' else 'public'
+            )
+        case 2:
+            csv_postgres_util = csv_to_postgres()
+            print('Make sure the csv file is formatted as follows:')
+            print('Header(Column to be updated) | Header(Update Value Header)')
+            print('Ensure search value is the left colum and update value is the second column')
+            csv_path = csv_postgres_util.get_csv_file_path().strip()
+            connection_string = csv_postgres_util.get_db_connection_string()
+            database_table = input('Enter the name of the database table: ')
+            schema: str = input('Enter the target schema, if nothing is entered public will be the default schema: ')
+            csv_postgres_util.csv_to_postgres_update_one_column(
+                db_connection_string=connection_string,
+                csv_file_path=csv_path,
+                database_table=database_table,
+                schema=schema if len(schema) >= 1 and schema.strip() != '' else 'public' 
             )
         case _:
             print("Yaay, I didn't have to do anything")	
-
-
-
-
-
-
-
-# table_name = 'your_table_name'
-
-# for index, row in df_excel.iterrows():
-#     employer = row['Employer']
-#     sector = row['Sector']
-
-#     # Execute an UPDATE statement for each row
-#     with db_connection.cursor() as cursor:
-#         update_query = f"UPDATE {table_name} SET Sector = {sector} WHERE Employer = '{employer}'"
-#         cursor.execute(update_query)
-
-# # Commit the changes
-# db_connection.commit()
-
-
--- utils/csv_postgres.py
+++ utils/csv_postgres.py
@@ -1,11 +1,61 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer, update
+from sqlalchemy.orm import DeclarativeBase
 
+
+class Base(DeclarativeBase):
+    pass
 
 class csv_to_postgres:
 
+    # TODO: Self to initialize DB Connection String
+    # def __init__(self):
+    #     pass
 
-    def is_valid_csv(csv_file_path):
+    def get_csv_file_path(self):
+        print('Enter the file path')
+        incorrect_path = True
+        csv_path = ''
+        while incorrect_path:
+            print('Paste in/ Enter the path to the csv file')
+            print('The format should be like: C:\\\\file.csv')
+            csv_path = input('Paste in/ Enter the path to the csv file: ')
+            if self.is_valid_csv(csv_path.strip()):
+                incorrect_path = False
+                break
+            else:
+                print(f'{csv_path} is an invalid path')
+        return csv_path
+
+    def get_db_connection_string(self):
+        connection_string = ''
+        incorrect_connection_string: bool = True
+        while incorrect_connection_string:
+            db_host = input('Enter the database host: ')
+            db_username = input('Enter the database username: ')
+            db_password = input('Enter the database password: ')
+            db_name = input('Enter the database name: ')
+            db_port = 5432
+            invalid_port = True
+            while invalid_port:
+                try:
+                    db_port = int(input('Enter the database port: '))
+                    invalid_port = False
+                    break
+                except ValueError:
+                    print('The port number entered is invalid')
+            is_valid_connection_string, connection_string, connection_error_thrown \
+                = self.test_connection_to_postgres_db(
+                {'host': f'{db_host}', 'u_name': f'{db_username}', 'pass': f'{db_password}',
+                 'db_name': f'{db_name}',
+                 'port': f'{db_port}'})
+            if is_valid_connection_string:
+                break
+            else:
+                print(f'The connection was not made due to error {connection_error_thrown}')
+        return connection_string
+
+    def is_valid_csv(self, csv_file_path):
         if len(csv_file_path) < 5:
             return False
         file_extension = csv_file_path[-4:]
@@ -13,22 +63,23 @@ class csv_to_postgres:
             return False
         try:
             file = open(csv_file_path)
-            file.close()        
+            file.close()
         except:
             return False
         return True
-    
-    def test_connection_to_postgres_db(connection_dict: dict):
+
+    def test_connection_to_postgres_db(self, connection_dict: dict):
         postgres_db_conn_str = f'postgresql+psycopg2://{connection_dict["u_name"]}:{connection_dict["pass"]}@{connection_dict["host"]}:{connection_dict["port"]}/{connection_dict["db_name"]}'
         engine = create_engine(postgres_db_conn_str)
         try:
             connection = engine.connect()
             connection.close()
         except Exception as e:
-                return (False, postgres_db_conn_str,  e)
-        return (True, postgres_db_conn_str,  None)
+            return False, postgres_db_conn_str, e
+        return True, postgres_db_conn_str, None
 
-    def csv_to_postgres_append(csv_file_path: str, db_connection_string: str, mapping: dict, database_table: str, schema:str):
+    def csv_to_postgres_append(self, csv_file_path: str, db_connection_string: str, mapping: dict, database_table: str,
+                               schema: str):
         # Read the CSV file into a Pandas DataFrame
         csv_file = csv_file_path
         df = pd.read_csv(csv_file)
@@ -55,3 +106,88 @@ class csv_to_postgres:
             print('Excel to DB Migration was successful')
         except Exception as e:
             print(f'Excel to DB Migration was not successful. Migration failed with error:{e}')
+
+    def get_table_model(self):
+        columns_dict = {}
+        primary_key_found = False
+        primary_key = ''
+        in_progress = True
+        print('You will be prompted to enter columns and their data types')
+        print('Current supported data types are integer and string')
+        while in_progress:
+            column_name = input('Enter column name: ')
+            column_data_type = input('Enter column datatype. i for integer or s for string: ')
+            column_data_type_transformed = String if column_data_type == 's' else Integer
+            column_data_type_transformed_str = 'String' if column_data_type == 's' else 'Integer'
+            if not primary_key_found:
+                primary_key = input('Is this column a primary key? Enter y for yes anything else for no: ')
+                primary_key = column_name if primary_key == 'y' else ''
+                primary_key_found = True
+            valid_column = input(f'Are you happy with column: {column_name}: {column_data_type_transformed_str}?'
+                                 f' Enter y for yes anything else for no: ')
+            if valid_column.strip() == 'Y' or valid_column.strip() == 'y':
+                columns_dict[column_name] = column_data_type_transformed
+            else:
+                print('The column entered will be disregarded')
+                if primary_key == column_name:
+                    primary_key = ''
+                    primary_key_found = False
+            user_done = input('Are you done? Enter y for yes and anything else for no: ')
+            if (user_done == 'y' and primary_key_found) or (user_done == 'Y' and primary_key_found):
+                in_progress = False
+            elif (user_done == 'y' and not primary_key_found) or (user_done == 'Y' and not primary_key_found):
+                print('You did not provide a primary key')
+                print('The process is starting again...')
+                columns_dict = {}
+        return columns_dict, primary_key
+
+    def create_dynamic_class(self, table_name: str, columns: dict, schema, primary_key):
+        attributes = {'__tablename__': table_name}
+        # Define the columns dynamically
+        for column_name, column_type in columns.items():
+            attributes[column_name] = Column(column_type, primary_key=True if primary_key == column_name else False)
+
+        # Set schema if provided
+        if schema:
+            attributes['__table_args__'] = {'schema': schema}
+
+        # Create the class using type
+        dynamic_class = type(table_name.capitalize(), (Base,), attributes)
+
+        return dynamic_class
+
+    def csv_to_postgres_update_one_column(self, csv_file_path: str, db_connection_string: str, database_table: str,
+                                          schema: str):
+        engine = create_engine(db_connection_string)
+        columns, primary_key = self.get_table_model()
+        TableModel = self.create_dynamic_class(
+            table_name=database_table,
+            columns=columns,
+            schema=schema,
+            primary_key=primary_key
+        )
+        df = pd.read_csv(csv_file_path)
+        column_to_be_searched = df.columns[0]
+        column_to_update = df.columns[1]
+        search_column_column_element = getattr(TableModel, column_to_be_searched)
+        update_column_column_element = getattr(TableModel, column_to_update)
+        update_dict = {}
+        for index, row in df.iterrows():
+            to_be_updated = row[column_to_be_searched]
+            update_value = row[column_to_update]
+            update_dict[to_be_updated] = update_value
+        print(f'Number of values to be updated = {len(update_dict.keys())}')
+        count = 0
+        with engine.connect() as conn:
+            for search, value in update_dict.items():
+                count += 1
+                print(f'On record {count}')
+                try:
+                    conn.execute(
+                        update(TableModel)
+                        .where(search_column_column_element == search)
+                        .values({update_column_column_element: value}))
+                    conn.commit()
+                except Exception as e:
+                    print(f'Record {search} could not be updated due to error {e}')
+        print('Update is done')
